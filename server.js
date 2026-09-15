d1f0e1ac342c8f6d7545a4900be28bc2240dc925
Possi Gee <possigee96@gmail.com>
2026-09-15 06:54:47 +0000

    Fix Vercel entrypoint: import express and export app in server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-require('./backend/server');
+const express = require('express');
+const { app, start } = require('./backend/server');
+
+if (require.main === module) {
+  start().catch((err) => {
+    console.error('Failed to start server:', err);
+    process.exitCode = 1;
+  });
+}
+
+module.exports = app;
